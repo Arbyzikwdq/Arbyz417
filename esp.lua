f04a17d0000fdf668e575b8c52478152705b76b0
Arbyzikwdq <139893947+Arbyzikwdq@users.noreply.github.com>
2023-07-19 00:10:48 +0400

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,2 +1,18 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Arbyzikwdq/Arbyz417/main/esp.lua"))()
-local Window = Library.CreateLib("Youtube" , "DarkTheme")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Arbyzikwdq/Arbyz417/main/esp.lua"))()
+
+local colors = {
+
+SchemeColor = Color3.fromRGB(150, 72, 148),
+
+Background = Color3.fromRGB(15,15,15),
+
+Header = Color3.fromRGB(15,15,15),
+
+TextColor = Color3.fromRGB(255,255,255),
+
+ElementColor = Color3.fromRGB(20, 20, 20)
+}
+
+local Window = Library.CreateLib("Name")
+
+local Tab = Window:NewTab("main")
