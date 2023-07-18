77ff1dbebf5c40c6f6322d6cc02dd7413cc9abd3
Arbyzikwdq <139893947+Arbyzikwdq@users.noreply.github.com>
2023-07-19 00:50:39 +0400

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Arbyzikwdq/Arbyz417/main/UI_Template_1%20(1)%20(1).txt"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Arbyzikwdq/Ui/main/UI_Template_1%20(1)%20(1).txt"))()
 
 local colors = {
 
