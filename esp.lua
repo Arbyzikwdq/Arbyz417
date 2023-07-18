25f13b2d8c0e1c66fae953a715baef16c2731b4b
Arbyzikwdq <139893947+Arbyzikwdq@users.noreply.github.com>
2023-07-18 21:08:39 +0400

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,5 +1,2 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Arbyzikwdq/Arbyz417/main/esp.lua"))()
-
-local Window = Library.CreateLib("Name", "RJTheme3")
-
-local Tab = Window:NewTab("Main")
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Arbyzikwdq/Arbyz417/main/esp.lua", true))()
+local UI1 = library:CreateWindow("Test UI")
