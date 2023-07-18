b1b620791075f8e2600f2966c0dad21270a35729
Arbyzikwdq <139893947+Arbyzikwdq@users.noreply.github.com>
2023-07-18 21:38:33 +0400

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,4 +1,2 @@
-local player = game.Players.LocalPlayer
-
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Arbyzikwdq/Arbyz417/main/esp.lua", true))()
-local UI1 = library:CreateWindow("Test")
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Arbyzikwdq/Arbyz417/main/esp.lua"))()
+local Window = Library.CreateLib("Youtube" , "DarkTheme")
