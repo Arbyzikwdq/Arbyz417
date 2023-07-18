69ba7252cf21a171fa1101230f0c90f44a5a2998
Arbyzikwdq <139893947+Arbyzikwdq@users.noreply.github.com>
2023-07-19 01:15:17 +0400

    Update Arbyz.lua

--- Arbyz.lua
+++ Arbyz.lua
@@ -10,7 +10,7 @@ Header = Color3.fromRGB(15,15,15),
 
 TextColor = Color3.fromRGB(255,255,255),
 
-ElementColor = Color3.fromRGB(20, 20, 20)
+ElementColor = Color3.fromRGB(153, 255, 51)
 }
 
 local Window = Library.CreateLib("ArbyzTap 0.1")
