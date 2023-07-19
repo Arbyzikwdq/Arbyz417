3765c1b3d52aa6501a73500972f08dff36e27d84
Arbyzikwdq <139893947+Arbyzikwdq@users.noreply.github.com>
2023-07-19 12:31:31 +0400

    Update Arbyz.lua

--- Arbyz.lua
+++ Arbyz.lua
@@ -18,3 +18,9 @@ local Window = Library.CreateLib("ArbyzTap 0.1")
 local Tab = Window:NewTab("Speed")
 
 local Tab = Window:NewTab("test")
+
+local Section = Tab:NewSection("Section Name")
+
+Section:NewSlider("SliderText", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
